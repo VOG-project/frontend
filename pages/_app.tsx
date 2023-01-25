b821d77bbe1effd59579b860ebb69a83faa42c72
origin1508 <origin1508@gmail.com>
2023-01-25 18:28:17 +0900

    feat: import ErrorBoundary, ThemeProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
-import GlobalStyle from "@/styles/GlobalStyle";
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "@emotion/react";
+import theme from "@/styles/theme";
+import ErrorBoundary from "@/components/ErrorBoundary/ErrorBoundary";
+import GlobalStyle from "@/styles/GlobalStyle";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <GlobalStyle />
-      <Component {...pageProps} />
+      <ErrorBoundary>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </ErrorBoundary>
     </>
   );
 }
