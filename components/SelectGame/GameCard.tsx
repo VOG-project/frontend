d9949a4473a9e1acfd9f0cd4d2f7bffe9330106f
origin1508 <origin1508@gmail.com>
2023-02-01 23:17:56 +0900

    feat: GameCard

---
+++ components/SelectGame/GameCard.tsx
@@ -0,0 +1,36 @@
+import Image, { StaticImageData } from "next/image";
+import tw from "twin.macro";
+
+interface GameCardProps {
+  name: string;
+  image: StaticImageData;
+  logo: StaticImageData;
+}
+
+const GameCard = ({ name, image, logo }: GameCardProps) => {
+  return (
+    <GameCardContainer>
+      <GameImage
+        src={image}
+        alt={name}
+        quality={100}
+        style={{ objectFit: "cover" }}
+      />
+      <GameLogo src={logo} alt={name} quality={100} />
+    </GameCardContainer>
+  );
+};
+
+export default GameCard;
+
+const GameCardContainer = tw.div`
+flex flex-col gap-8 w-64 h-[50rem] cursor-pointer
+`;
+
+const GameImage = tw(Image)`
+  w-full h-full bg-gray-500
+`;
+
+const GameLogo = tw(Image)`
+  w-full h-24 bg-transparent
+`;
