546e55342c2949c74a8c438858a01fa9c8c14fa6
origin1508 <origin1508@gmail.com>
2023-02-01 23:15:40 +0900

    fix: NAV_MENU 추가

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
 import tw from "twin.macro";
 import Header from "../common/Header";
 import UserCard from "../common/UserCard";
 import { getIcons } from "../icons";
+
+const NAV_MENU = [
+  { name: "채팅", href: "/chat", icon: getIcons("chat", 34) },
+  { name: "커뮤니티", href: "/community", icon: getIcons("cardList", 34) },
+];
 
 const Sidebar = () => {
   return (
@@ -14,12 +20,17 @@ const Sidebar = () => {
         </DropdownWrapper>
       </Profile>
       <SidebarMenu>
-        <SidebarItem>
-          <ItemIcon>{getIcons("chat", 30)}</ItemIcon>채팅
-        </SidebarItem>
-        <SidebarItem>
-          <ItemIcon>{getIcons("cardList", 30)}</ItemIcon>커뮤니티
-        </SidebarItem>
+        {NAV_MENU.map((menu) => {
+          const { name, href, icon } = menu;
+          return (
+            <SidebarItem key={name}>
+              <SidebarLink href={href}>
+                <ItemIcon>{icon}</ItemIcon>
+                {name}
+              </SidebarLink>
+            </SidebarItem>
+          );
+        })}
       </SidebarMenu>
     </SidebarContainer>
   );
@@ -28,7 +39,7 @@ const Sidebar = () => {
 export default Sidebar;
 
 const SidebarContainer = tw.nav`
-  fixed flex flex-col w-64 h-full p-4 border-r border-neutral-700
+  fixed flex flex-col w-64 h-full p-4 border-r border-neutral-700 text-xl
 `;
 
 const Profile = tw.div`
@@ -44,13 +55,17 @@ const DropdwonIcon = tw.div`
 `;
 
 const SidebarMenu = tw.ul`
-  p-4 text-xl font-semibold
+  p-4 font-semibold
 `;
 
 const SidebarItem = tw.li`
-  flex w-full my-2
+  w-full my-2
+`;
+
+const SidebarLink = tw(Link)`
+  flex flex-row items-center w-full
 `;
 
 const ItemIcon = tw.div`
-  mr-2
+  mr-4
 `;
