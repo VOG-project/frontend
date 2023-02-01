608204f2e47c1f3193efb584c3a6ac6fc5c217ea
origin1508 <origin1508@gmail.com>
2023-02-01 23:18:26 +0900

    feat: constants games

---
+++ constants/games.ts
@@ -0,0 +1,11 @@
+import yasuo from "@/public/image/yasou.webp";
+import zett from "@/public/image/valorant_jett.jpg";
+import LoL_logo from "@/public/image/LoL_logo.png";
+import valorant_logo from "@/public/image/valorant_logo2.png";
+
+const GAMES = [
+  { name: "League of Legends", image: yasuo, logo: LoL_logo },
+  { name: "Valorant", image: zett, logo: valorant_logo },
+];
+
+export default GAMES;
