bb8b51ccf6a4039baac0c17403b01a36887fba93
origin1508 <origin1508@gmail.com>
2023-02-01 23:19:54 +0900

    feat: down icons 추가

--- components/icons/index.tsx
+++ components/icons/index.tsx
@@ -1,4 +1,5 @@
 import { BsChat, BsCardList } from "react-icons/bs";
+import { RxTriangleDown } from "react-icons/rx";
 
 export function getIcons(name: string, size: number, color?: string) {
   switch (name) {
@@ -6,6 +7,8 @@ export function getIcons(name: string, size: number, color?: string) {
       return <BsChat size={size} color={color} />;
     case "cardList":
       return <BsCardList size={size} color={color} />;
+    case "down":
+      return <RxTriangleDown size={size} color={color} />;
     default:
       console.log("Not implemented!");
       return;
