9f375c93455e18fff556dc32af1ba1a7a5c63b6a
origin1508 <origin1508@gmail.com>
2023-02-03 21:13:22 +0900

    fix: header 두가지 종류로 가능하게 수정

--- components/common/Header/Header.tsx
+++ components/common/Header/Header.tsx
@@ -1,22 +1,29 @@
 import { ReactNode } from "react";
-import tw from "twin.macro";
+import tw, { styled } from "twin.macro";
 
 interface HeaderProps {
+  singleMode: boolean;
+  title: string;
   children: ReactNode;
 }
 
-const Header = ({ children }: HeaderProps) => {
+const Header = ({ singleMode = true, title, children }: HeaderProps) => {
   return (
-    <HeaderContainer>
-      <HeaderTitle>{children}</HeaderTitle>
+    <HeaderContainer mode={singleMode}>
+      <HeaderTitle>{title}</HeaderTitle>
+      {!singleMode && children}
     </HeaderContainer>
   );
 };
 
 export default Header;
 
-const HeaderContainer = tw.header`
-  h-16 border-b border-neutral-700
-`;
+const HeaderContainer = styled.header<{ mode: boolean }>(({ mode }) => [
+  mode
+    ? tw`h-16 border-b border-neutral-700`
+    : tw`flex justify-between items-center w-full h-20`,
+]);
 
-const HeaderTitle = tw.h2``;
+const HeaderTitle = tw.h2`
+  text-4xl font-semibold
+`;
