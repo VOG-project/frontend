f18676e706623c6d544e9c26dea46b68cfee3db9
origin1508 <origin1508@gmail.com>
2023-02-16 23:23:43 +0900

    fix: width props 추가

--- components/common/Button/Button.tsx
+++ components/common/Button/Button.tsx
@@ -1,19 +1,29 @@
-import tw from "twin.macro";
+import tw, { styled, css } from "twin.macro";
 import { ReactNode } from "react";
 
 interface ButtonProps {
   type?: "button" | "submit";
+  width?: number;
   children: ReactNode;
+  onClick?: () => void;
 }
 
-const Button = ({ type = "button", children }: ButtonProps) => {
+const Button = ({ type = "button", width, children, onClick }: ButtonProps) => {
   return (
     <>
-      <StyledButton type={type}>{children}</StyledButton>
+      <StyledButton type={type} width={width} onClick={onClick}>
+        {children}
+      </StyledButton>
     </>
   );
 };
 
 export default Button;
 
-const StyledButton = tw.button`my-4 h-10 rounded bg-sky-900 text-white`;
+const StyledButton = styled.button<{ width?: number }>(({ width }) => [
+  tw`my-4 h-10 rounded bg-sky-500 text-white`,
+  width &&
+    css`
+      width: ${width}px;
+    `,
+]);
