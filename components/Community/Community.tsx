14b5d3089dd7061324a26b98f6aaa82d680ece31
origin1508 <origin1508@gmail.com>
2023-02-16 23:34:27 +0900

    fix: title 수정

--- components/Community/Community.tsx
+++ components/Community/Community.tsx
@@ -1,17 +1,24 @@
 import tw from "twin.macro";
+import { useRouter } from "next/router";
 import MainLayout from "../layout/MainLayout";
 import Header from "../common/Header";
 import Search from "../common/Search";
 import Navigation from "./Navigation";
 import Contents from "./Contents";
+import { NAV_MENU } from "./Navigation/Navigation";
 
 const Community = () => {
+  const router = useRouter();
+  const { category } = router.query;
+  const title = NAV_MENU.find((menu) => {
+    return menu.href === category;
+  });
   return (
     <MainLayout>
       <CommunityWrapper>
         <Navigation />
         <CommunityContainer>
-          <Header title="자유게시판">
+          <Header title={title ? title.name : ""}>
             <Search />
           </Header>
           <Contents />
