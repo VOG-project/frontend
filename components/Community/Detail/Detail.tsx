3087004e40a7b7eeb5115405ce38969b62442901
origin1508 <origin1508@gmail.com>
2023-02-17 23:41:03 +0900

    fix: detail 수정

--- components/Community/Detail/Detail.tsx
+++ components/Community/Detail/Detail.tsx
@@ -1,15 +1,21 @@
+import { useRouter } from "next/router";
 import tw from "twin.macro";
 import MainLayout from "@/components/layout/MainLayout";
 import Navigation from "../Navigation";
 import Header from "@/components/common/Header";
+import { getTitle } from "@/utils/getTitle";
 
 const Detail = () => {
+  const router = useRouter();
+  const query = router.query;
+  const category = typeof query.category === "string" ? query.category : "";
+  const title = getTitle(category);
   return (
     <MainLayout>
       <DetailWrapper>
-        <Navigation />
+        <Navigation category={category} />
         <DetailContainer>
-          <Header title="자유게시판"></Header>
+          <Header title={title}></Header>
         </DetailContainer>
       </DetailWrapper>
     </MainLayout>
