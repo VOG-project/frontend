811cb4b20d1c32f291a839318c3f2066219e1b68
origin1508 <origin1508@gmail.com>
2023-02-20 23:57:34 +0900

    fix: Comment 수정

--- components/Community/Detail/Comments/Comment.tsx
+++ components/Community/Detail/Comments/Comment.tsx
@@ -3,13 +3,29 @@ import tw from "twin.macro";
 const Comment = () => {
   return (
     <CommentContainer>
-      <CommentTitle>댓글 1</CommentTitle>
+      <CommentAuthor>
+        사부로
+        <CommentTime>2023.01.01 PM 09:45</CommentTime>
+      </CommentAuthor>
+      <CommentText>저랑 해요</CommentText>
     </CommentContainer>
   );
 };
 
 export default Comment;
 
-const CommentContainer = tw.div``;
+const CommentContainer = tw.div`
+  px-4 py-2
+`;
 
-const CommentTitle = tw.div``;
+const CommentAuthor = tw.div`
+  flex
+`;
+
+const CommentTime = tw.div`
+  ml-2 text-zinc-400
+`;
+
+const CommentText = tw.p`
+  mt-2
+`;
