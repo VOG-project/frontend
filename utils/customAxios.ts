b2c340e9ebf11ad05f70392f39607cc4aa1423c0
origin1508 <origin1508@gmail.com>
2023-02-21 21:57:37 +0900

    feat: customAxios

---
+++ utils/customAxios.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const customAxios = () => {
+  const instance = axios.create({
+    baseURL: process.env.ENDPOINT,
+    timeout: 3000,
+    headers: { "Content-Type": "application/json" },
+  });
+
+  instance.interceptors.request.use(
+    (request) => {
+      return request;
+    },
+    (error) => {
+      throw error;
+    }
+  );
+
+  instance.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      throw error;
+    }
+  );
+
+  return instance;
+};
+
+export default customAxios;
