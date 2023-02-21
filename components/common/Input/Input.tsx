b48791f37eea33dab996f4f3fc26386fe470966d
origin1508 <origin1508@gmail.com>
2023-02-21 23:14:43 +0900

    fix: radio 값과 width 값을 받을 수 있게 수정

--- components/common/Input/Input.tsx
+++ components/common/Input/Input.tsx
@@ -1,25 +1,43 @@
-import tw from "twin.macro";
+import tw, { styled, css } from "twin.macro";
 import { UseFormRegisterReturn } from "react-hook-form";
 
 interface InputProps {
   register: UseFormRegisterReturn<
-    "email" | "password" | "confirmPassword" | "nickname"
+    "email" | "password" | "confirmPassword" | "nickname" | "sex"
   >;
-  type?: "default" | "password";
   placeholder?: string;
+  value?: string;
+  width?: number;
+  type?: "default" | "password" | "radio";
 }
 
-const Input = ({ register, placeholder, type = "default" }: InputProps) => {
+const Input = ({
+  register,
+  placeholder,
+  value,
+  width,
+  type = "default",
+}: InputProps) => {
   return (
     <>
-      <StyledInput {...register} placeholder={placeholder} type={type} />
+      <StyledInput
+        {...register}
+        placeholder={placeholder}
+        value={value}
+        width={width}
+        type={type}
+      />
     </>
   );
 };
 
 export default Input;
 
-const StyledInput = tw.input`
-px-2 w-full h-10 bg-transparent outline-0
-placeholder:text-gray-600
-`;
+const StyledInput = styled.input<{ width?: number }>(({ width }) => [
+  tw`px-2 w-full h-10 bg-transparent outline-0
+  placeholder:text-gray-600`,
+  width &&
+    css`
+      width: ${width}rem;
+    `,
+]);
