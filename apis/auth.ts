c1dd56f3623f79592828f4faf5ce11c53e45be58
origin1508 <origin1508@gmail.com>
2023-02-27 21:22:33 +0900

    fix: LoginRequest 타입지정

--- apis/auth.ts
+++ apis/auth.ts
@@ -1,24 +1,23 @@
 import customAxios from "@/utils/customAxios";
 
-const loginRequest = async ({
-  email,
-  password,
-}: {
+export interface LoginRequest {
   email: string;
   password: string;
-}) => {
+}
+
+const loginRequest = async ({ email, password }: LoginRequest) => {
   const res = await customAxios().post("/auth/login", {
     email,
     password,
   });
 
-  return res;
+  return res.data;
 };
 
 const logoutRequest = async () => {
   const res = await customAxios().delete("/auth/logout");
 
-  return res;
+  return res.data;
 };
 
 export { loginRequest, logoutRequest };
