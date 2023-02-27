e4c623aead86463aa6a58fba4258b707f08b057c
origin1508 <origin1508@gmail.com>
2023-02-27 21:22:55 +0900

    fix: SignUpRequest 타입 지정

--- apis/user.ts
+++ apis/user.ts
@@ -1,23 +1,25 @@
 import customAxios from "@/utils/customAxios";
 
+export interface SignUpRequest {
+  email: string;
+  password: string;
+  nickname: string;
+  sex: string;
+}
+
 const signUpRequest = async ({
   email,
   password,
   nickname,
   sex,
-}: {
-  email: string;
-  password: string;
-  nickname: string;
-  sex: string;
-}) => {
+}: SignUpRequest) => {
   const res = await customAxios().post("/users/register", {
     email,
     password,
     nickname,
     sex,
   });
-  return res;
+  return res.data;
 };
 
 export { signUpRequest };
