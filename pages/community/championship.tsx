a210e1b8066605325600e9e171f7b488b7e5a75f
origin1508 <origin1508@gmail.com>
2023-03-09 21:16:56 +0900

    fix: 이름변경 Championship

---
+++ pages/community/championship.tsx
@@ -0,0 +1,10 @@
+import Community from "@/components/Community";
+
+const Championship = () => {
+  return (
+    <>
+      <Community category="championship" />
+    </>
+  );
+};
+export default Championship;
