d0b76a730dc12f189c3e3ff8ed6669ca6af53f7f
origin1508 <origin1508@gmail.com>
2023-03-11 00:20:22 +0900

    fix: keyframes 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,24 @@ module.exports = {
         primary: "#AA9377",
         secondary: "#CB1944",
       },
+      keyframes: {
+        slideRight: {
+          "0%": {
+            transform: "translateX(-100%)",
+          },
+          "100%": {
+            transform: "translateX(0)",
+          },
+        },
+        slideLeft: {
+          "0%": {
+            transform: "translateX(100%)",
+          },
+          "100%": {
+            transform: "translateX(0)",
+          },
+        },
+      },
     },
   },
   plugins: [],
