449f3c9b43cb83d5513308f1c1d66a4b88bfc63a
origin1508 <origin1508@gmail.com>
2023-03-10 22:02:51 +0900

    fix: position props를 받을 수 있게 수정

--- components/common/Button/Button.tsx
+++ components/common/Button/Button.tsx
@@ -5,6 +5,13 @@ interface ButtonProps {
   type?: "button" | "submit";
   width?: number;
   transparent?: boolean;
+  position?: {
+    type: "relative" | "absolute";
+    top?: string;
+    bottom?: string;
+    left?: string;
+    right?: string;
+  };
   children: ReactNode;
   onClick?: () => void;
 }
@@ -13,6 +20,7 @@ const Button = ({
   type = "button",
   width,
   transparent,
+  position,
   children,
   onClick,
 }: ButtonProps) => {
@@ -22,6 +30,7 @@ const Button = ({
         type={type}
         width={width}
         transparent={transparent}
+        position={position}
         onClick={onClick}
       >
         {children}
@@ -32,16 +41,32 @@ const Button = ({
 
 export default Button;
 
-const StyledButton = styled.button<{ width?: number; transparent?: boolean }>(
-  ({ width, transparent }) => [
-    tw`my-4 h-10 rounded bg-sky-500 text-white`,
-    width &&
-      css`
-        width: ${width}rem;
-      `,
-    transparent &&
-      css`
-        background-color: transparent;
-      `,
-  ]
-);
+const StyledButton = styled.button<{
+  width?: number;
+  transparent?: boolean;
+  position?: {
+    type: "relative" | "absolute";
+    top?: string;
+    bottom?: string;
+    left?: string;
+    right?: string;
+  };
+}>(({ width, transparent, position }) => [
+  tw`my-4 h-10 rounded bg-primary text-white`,
+  width &&
+    css`
+      width: ${width}rem;
+    `,
+  transparent &&
+    css`
+      background-color: transparent;
+    `,
+  position &&
+    css`
+      position: ${position.type};
+      top: ${position.top};
+      bottom: ${position.bottom};
+      left: ${position.left};
+      right: ${position.right};
+    `,
+]);
