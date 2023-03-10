bb951ddd7045645c06a1ee1710f73ada2146ab3a
origin1508 <origin1508@gmail.com>
2023-03-11 00:41:15 +0900

    fix: History 스타일 수정

--- components/MyPage/MyPageCard/History.tsx
+++ components/MyPage/MyPageCard/History.tsx
@@ -12,16 +12,20 @@ const History = () => {
     <HistoryContainer>
       <HistoryMenu>
         <HistoryTitle>활동 내역</HistoryTitle>
-        <HistoryList onClick={handleHistoryListClick}>
-          내가 작성한 글
+        <HistoryList>
+          <HistoryListItem onClick={handleHistoryListClick}>
+            내가 작성한 글
+          </HistoryListItem>
+          <HistoryListItem onClick={handleHistoryListClick}>
+            내가 작성한 댓글
+          </HistoryListItem>
+          <HistoryListItem onClick={handleHistoryListClick}>
+            참여한 채팅 목록
+          </HistoryListItem>
+          <HistoryListItem onClick={handleHistoryListClick}>
+            채팅 내역
+          </HistoryListItem>
         </HistoryList>
-        <HistoryList onClick={handleHistoryListClick}>
-          내가 작성한 댓글
-        </HistoryList>
-        <HistoryList onClick={handleHistoryListClick}>
-          참여한 채팅 목록
-        </HistoryList>
-        <HistoryList onClick={handleHistoryListClick}>채팅 내역</HistoryList>
       </HistoryMenu>
       <HistoryContentContainer isOpen={isOpen}>
         <HistoryContent>
@@ -45,13 +49,21 @@ const HistoryContainer = tw.section`
   absolute flex left-0 w-1/3 h-full border-r border-neutral-700
 `;
 
-const HistoryMenu = tw.ul`
-  w-full h-full text-center
+const HistoryMenu = tw.div`
+  w-full h-full py-6 px-10 text-center
 `;
 
-const HistoryList = tw.li``;
+const HistoryTitle = tw.h2`
+  py-6 border-b border-white text-3xl
+`;
 
-const HistoryTitle = tw.h2``;
+const HistoryList = tw.ul`
+  w-1/2 mx-auto mt-20 text-xl list-disc
+`;
+
+const HistoryListItem = tw.li`
+  mt-8 cursor-pointer
+`;
 
 const HistoryContentContainer = styled.div<{ isOpen: boolean }>(
   ({ isOpen }) => [
