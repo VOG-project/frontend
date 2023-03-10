b18abf83f82cbabd6e15856d73b1841dfa6339f9
origin1508 <origin1508@gmail.com>
2023-03-10 22:02:33 +0900

    fix: getServerSideProps 추가

--- pages/community/humor.tsx
+++ pages/community/humor.tsx
@@ -1,10 +1,21 @@
+import { GetServerSideProps } from "next";
 import Community from "@/components/Community";
+import { getPostsRequest } from "@/apis/community";
+import { CommunityProps } from "@/types/community";
 
-const Humor = () => {
+const Humor = ({ data }: CommunityProps) => {
   return (
     <>
-      <Community category="humor" />
+      <Community data={data} category="humor" />
     </>
   );
 };
 export default Humor;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const category = context.resolvedUrl.split("/")[2];
+  const res = await getPostsRequest(category, 1);
+
+  // 전체 페이지 갯수 요청...
+  return { props: { data: res } };
+};
