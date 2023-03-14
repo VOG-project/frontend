6c7b86a15a1b8da37de62a8a95e957d621b330d5
origin1508 <origin1508@gmail.com>
2023-03-15 01:20:37 +0900

    feat: ProfilePicEdit

---
+++ components/MyPage/MyPageCards/ProfilePicEdit.tsx
@@ -0,0 +1,47 @@
+import { useRef } from "react";
+import tw from "twin.macro";
+import Left from "@/components/common/MyPageCard/Left";
+import Right from "@/components/common/MyPageCard/Right";
+import Button from "@/components/common/Button";
+import { getIcons } from "@/components/icons";
+
+const ProfilePicEdit = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const handlePicButtonClick = () => {
+    if (inputRef.current) {
+      inputRef.current.click();
+    }
+  };
+  return (
+    <ProfilePicEditContainer>
+      <Left title="프로필이미지 변경" />
+      <Right>
+        <ProfilePicInput type={"file"} ref={inputRef}></ProfilePicInput>
+        <ProfilePicButton onClick={handlePicButtonClick}>
+          {getIcons("plus", 64)}
+        </ProfilePicButton>
+        <ProfilePicSumbitButton>
+          <Button width={8}>변경하기</Button>
+        </ProfilePicSumbitButton>
+      </Right>
+    </ProfilePicEditContainer>
+  );
+};
+
+export default ProfilePicEdit;
+
+const ProfilePicEditContainer = tw.div`
+  flex w-full
+`;
+
+const ProfilePicInput = tw.input`
+  hidden
+`;
+
+const ProfilePicButton = tw.button`
+  flex items-center justify-center w-32 h-32 m-auto rounded-full bg-zinc-600
+`;
+
+const ProfilePicSumbitButton = tw.div`
+  float-right
+`;
