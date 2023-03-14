1112905c177573162a4aa8fc34db98139a8e5de1
origin1508 <origin1508@gmail.com>
2023-03-15 01:21:35 +0900

    feat: MyPageCard 공용 컴포넌트 스타일

---
+++ components/common/MyPageCard/Left.tsx
@@ -0,0 +1,27 @@
+import tw from "twin.macro";
+
+interface LeftProps {
+  title: string;
+  description?: string;
+}
+
+const Left = ({ title, description }: LeftProps) => {
+  return (
+    <LeftContainer>
+      <Title>{title}</Title>
+      <Description>{description}</Description>
+    </LeftContainer>
+  );
+};
+
+export default Left;
+
+const LeftContainer = tw.div`
+  shrink-0 w-64 p-8 bg-zinc-900
+`;
+
+const Title = tw.h2`
+  text-xl
+`;
+
+const Description = tw.p``;
