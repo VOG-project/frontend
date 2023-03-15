c67ffd78027870dcfc60b64fa36d66a946926a85
origin1508 <origin1508@gmail.com>
2023-03-15 21:18:14 +0900

    fix: bgColor prop 추가

--- components/common/Input/Input.tsx
+++ components/common/Input/Input.tsx
@@ -2,12 +2,14 @@ import tw, { styled, css } from "twin.macro";
 import { UseFormRegisterReturn } from "react-hook-form";
 
 interface InputProps {
-  register: UseFormRegisterReturn<
+  register?: UseFormRegisterReturn<
     "email" | "password" | "confirmPassword" | "nickname" | "sex"
   >;
   placeholder?: string;
   value?: string;
   width?: number;
+  height?: number;
+  bgColor?: "gray";
   type?: "default" | "password" | "radio";
 }
 
@@ -16,6 +18,8 @@ const Input = ({
   placeholder,
   value,
   width,
+  height,
+  bgColor,
   type = "default",
 }: InputProps) => {
   return (
@@ -25,6 +29,8 @@ const Input = ({
         placeholder={placeholder}
         value={value}
         width={width}
+        height={height}
+        bgColor={bgColor}
         type={type}
       />
     </>
@@ -33,11 +39,22 @@ const Input = ({
 
 export default Input;
 
-const StyledInput = styled.input<{ width?: number }>(({ width }) => [
+const StyledInput = styled.input<{
+  width?: number;
+  height?: number;
+  bgColor?: string;
+}>(({ width, height, bgColor }) => [
   tw`px-2 w-full h-10 bg-transparent outline-0
-  placeholder:text-gray-600`,
+  placeholder:(text-stone-600)`,
   width &&
     css`
       width: ${width}rem;
     `,
+  height &&
+    css`
+      height: ${height}rem;
+    `,
+  bgColor &&
+    bgColor === "gray" &&
+    tw`m-4 bg-stone-700 rounded hover:bg-stone-600`,
 ]);
