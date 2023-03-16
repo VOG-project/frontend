2b0a46f4de5e8cb404ddd683c8e15c70e8f7b947
origin1508 <origin1508@gmail.com>
2023-03-17 02:38:23 +0900

    fix: currentPassword 값 추가

--- hooks/usePasswordEditForm.ts
+++ hooks/usePasswordEditForm.ts
@@ -1,6 +1,7 @@
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { PasswordEditValue } from "@/types/myPage";
 
 const usePasswordEditForm = () => {
   const passwordEditSchema = yup.object().shape({
@@ -17,6 +18,16 @@ const usePasswordEditForm = () => {
     confirmPassword: yup
       .string()
       .oneOf([yup.ref("password"), null], "비밀번호가 일치하지 않습니다."),
+    currentPassword: yup
+      .string()
+      .trim()
+      .min(8)
+      .max(15)
+      .matches(
+        /^^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^+\-=])(?=\S+$).*$/,
+        "대문자, 소문자, 특수문자, 숫자를 이용하여 8자이상 15자 이하 비밀번호를 입력해주세요."
+      )
+      .required("비밀번호를 입력해주세요"),
   });
 
   const {
@@ -24,11 +35,12 @@ const usePasswordEditForm = () => {
     handleSubmit,
     watch,
     formState: { errors, isDirty, isValid },
-  } = useForm({
+  } = useForm<PasswordEditValue>({
     mode: "onChange",
     defaultValues: {
       password: "",
       confirmPassword: "",
+      currentPassword: "",
     },
     resolver: yupResolver(passwordEditSchema),
   });
@@ -37,15 +49,21 @@ const usePasswordEditForm = () => {
 
   const watchConfirmPassword = watch("confirmPassword");
 
+  const watchCurrentPassword = watch("currentPassword");
+
   const passwordError = errors.password;
 
   const confirmPasswordError = errors.confirmPassword;
 
+  const currentPasswordError = errors.currentPassword;
+
   return {
     watchPassword,
     watchConfirmPassword,
+    watchCurrentPassword,
     passwordError,
     confirmPasswordError,
+    currentPasswordError,
     isDirty,
     isValid,
     register,
