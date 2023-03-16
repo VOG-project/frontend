b78639d18abf8b035012a927a501c64858d31f03
origin1508 <origin1508@gmail.com>
2023-03-17 02:36:20 +0900

    fix: Toast import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 import ErrorBoundary from "@/components/ErrorBoundary";
 import GlobalStyle from "@/styles/GlobalStyle";
+import Toast from "@/components/Toast";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <ErrorBoundary>
         <RecoilRoot>
           <GlobalStyle />
+          <Toast />
           <Component {...pageProps} />
         </RecoilRoot>
       </ErrorBoundary>
