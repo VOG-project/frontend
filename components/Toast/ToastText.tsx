5045edae4e23d12177ec9fc37573d14d855dc649
origin1508 <origin1508@gmail.com>
2023-03-17 02:37:10 +0900

    fix: 애니메이션 수정, uuid 추가

--- components/Toast/ToastText.tsx
+++ components/Toast/ToastText.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import tw, { styled } from "twin.macro";
 import useToast from "@/hooks/useToast";
 import { getIcons } from "@/components/icons";
@@ -6,28 +6,24 @@ import { getIcons } from "@/components/icons";
 interface ToastTextProps {
   type: "success" | "alert";
   text: string;
+  uuid: string;
 }
 
-const ToastText = ({ type, text }: ToastTextProps) => {
-  const [isActive, setIsActive] = useState(true);
+const ToastText = ({ type, text, uuid }: ToastTextProps) => {
   const { closeToast } = useToast();
 
   useEffect(() => {
     const timeout = setTimeout(() => {
-      setIsActive(false);
+      closeToast(uuid);
     }, 2000);
-    const timeout2 = setTimeout(() => {
-      closeToast();
-    }, 2500);
     return () => {
       clearTimeout(timeout);
-      clearTimeout(timeout2);
     };
   });
 
   return (
     <>
-      <StyledToastText type={type} isActive={isActive}>
+      <StyledToastText type={type}>
         {getIcons(type, 20)}
         {text}
       </StyledToastText>
@@ -37,13 +33,8 @@ const ToastText = ({ type, text }: ToastTextProps) => {
 
 export default ToastText;
 
-const StyledToastText = styled.p<{ type: string; isActive: boolean }>(
-  ({ type, isActive }) => [
-    tw`absolute flex items-center justify-center top-24 left-1/2 h-8 min-w-[10rem] rounded shadow`,
-    isActive
-      ? tw`animate-[slideIn_0.8s]`
-      : tw`animate-[slideOut_0.8s_forwards]`,
-    type === "success" && tw` bg-green-600`,
-    type === "alert" && tw`bg-red-600`,
-  ]
-);
+const StyledToastText = styled.p<{ type: string }>(({ type }) => [
+  tw`absolute flex items-center justify-center top-24 left-1/2 h-8 px-2 min-w-[10rem] rounded shadow animate-[slideInOut_2s_forwards]`,
+  type === "success" && tw` bg-green-600`,
+  type === "alert" && tw`bg-red-600`,
+]);
