cb028c3c8507c65f21822559a7731c45231ca35d
origin1508 <origin1508@gmail.com>
2023-03-22 23:14:51 +0900

    fix: keyDown, change 이벤트 함수로 정리

--- components/Chat/ChatRoom/ChatRoom.tsx
+++ components/Chat/ChatRoom/ChatRoom.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState, useRef, KeyboardEvent, ChangeEvent } from "react";
 import tw from "twin.macro";
 import useChatState from "@/hooks/useChatState";
 import MainLayout from "@/components/layout/MainLayout";
@@ -12,6 +12,26 @@ const ChatRoom = () => {
   const buttonRef = useRef<HTMLButtonElement>(null);
   const { chat } = useChatState();
   const { members, roomId } = chat;
+
+  const handleTextAreaKeyDown = (e: KeyboardEvent<HTMLTextAreaElement>) => {
+    if (!e.shiftKey && e.key === "Enter") {
+      e.preventDefault();
+      buttonRef.current?.click();
+    }
+    return;
+  };
+
+  const handleTextAreaChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    e.target.style.height = "auto";
+    const scrollHeight = e.target.scrollHeight;
+    const message = e.target.value.trim();
+
+    if (message) {
+      setMessage(message);
+    }
+
+    e.target.style.height = scrollHeight + "px";
+  };
 
   return (
     <MainLayout>
@@ -28,21 +48,8 @@ const ChatRoom = () => {
           <ChatTextAreaContainer>
             <ChatTextArea
               placeholder="메시지를 입력하세요"
-              onChange={(e) => {
-                e.target.style.height = "auto";
-                const message = e.target.value.trim();
-                if (message) {
-                  setMessage(message);
-                }
-                const scrollHeight = e.target.scrollHeight;
-                e.target.style.height = scrollHeight + "px";
-              }}
-              onKeyDown={(e) => {
-                if (!e.shiftKey && e.key === "Enter") {
-                  e.preventDefault();
-                  buttonRef.current?.click();
-                }
-              }}
+              onChange={handleTextAreaChange}
+              onKeyDown={handleTextAreaKeyDown}
               rows={1}
             ></ChatTextArea>
             <ChatBntContainer>
@@ -51,6 +58,7 @@ const ChatRoom = () => {
                 ref={buttonRef}
                 onClick={() => {
                   sendMessageEmit(message, chat.roomId, "test");
+                  setMessage("");
                 }}
               >
                 SEND
@@ -90,7 +98,7 @@ const ChatTextAreaContainer = tw.div`
 `;
 
 const ChatTextArea = tw.textarea`
-  shrink grow p-4 w-full bg-transparent overflow-hidden resize-none bg-gray-800
+  shrink grow p-4 w-full max-h-96 rounded-l-lg bg-stone-800 resize-none 
   focus:(outline-none placeholder-transparent)
 `;
 
@@ -98,5 +106,5 @@ const ChatBntContainer = tw.div`
 `;
 
 const ChatSubmitBtn = tw.button`
-  float-right w-20 h-full bg-primary
+  float-right w-20 h-full rounded-r-lg bg-primary
 `;
