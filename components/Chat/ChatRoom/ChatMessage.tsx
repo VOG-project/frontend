a27981e1a830cd84fdc210eb36df37ceef1ae4d2
origin1508 <origin1508@gmail.com>
2023-03-22 23:14:04 +0900

    feat: ChatMessage

---
+++ components/Chat/ChatRoom/ChatMessage.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import tw, { styled } from "twin.macro";
+
+const ChatMessage = ({ isSender }) => {
+  return (
+    <ChatMessageContainer isSender={true}>
+      <ChatProfilePic>
+        <ProfilePic
+          src="/image/valorant_jett.jpg"
+          alt="profilePic"
+          width={64}
+          height={64}
+        />
+      </ChatProfilePic>
+      <ChatContent>
+        <Nickname>test</Nickname>
+        <Content>안녕하세요</Content>
+      </ChatContent>
+    </ChatMessageContainer>
+  );
+};
+
+export default ChatMessage;
+
+const ChatMessageContainer = styled.div<{ isSender: boolean }>(
+  ({ isSender }) => [
+    tw`flex gap-4`,
+    isSender && tw`flex-row-reverse [& div]:text-right`,
+  ]
+);
+
+const ChatProfilePic = tw.div`
+  shrink-0
+`;
+
+const ProfilePic = tw(Image)`
+  w-16 h-16 rounded-full
+`;
+
+const ChatContent = tw.div``;
+
+const Nickname = tw.div`
+  m-2
+`;
+
+const Content = tw.p`
+  w-auto p-4 rounded-lg bg-zinc-800 break-all whitespace-pre-wrap
+`;
