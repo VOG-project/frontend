079c282a03a0823c4c14ec7d671915deef65a697
origin1508 <origin1508@gmail.com>
2023-03-22 16:24:58 +0900

    feat: chat 관련 type

---
+++ types/chat.ts
@@ -0,0 +1,26 @@
+import { ParsedUrlQuery } from "querystring";
+
+export interface ChatQuery extends ParsedUrlQuery {
+  id: string;
+}
+
+export interface ChatRoom {
+  createdAt: string;
+  updatedAt: string;
+  roomId: string;
+  title: string;
+  currentMember: number;
+  maximumMember: number;
+  no: number;
+}
+
+export interface RoomListProps {
+  roomList: ChatRoom[];
+}
+
+export interface ChatProps {
+  data: {
+    success: boolean;
+    result: ChatRoom[];
+  };
+}
