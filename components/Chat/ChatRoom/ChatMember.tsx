5dc997a7165b2497880d486f68067d9c597cd7eb
origin1508 <origin1508@gmail.com>
2023-03-22 23:13:54 +0900

    fix: 방나가기 버튼 구현, 스타일 수정

--- components/Chat/ChatRoom/ChatMember.tsx
+++ components/Chat/ChatRoom/ChatMember.tsx
@@ -1,8 +1,15 @@
+import { useRouter } from "next/router";
 import Image from "next/image";
 import tw from "twin.macro";
 import Header from "@/components/common/Header";
+import Button from "@/components/common/Button";
+import { getIcons } from "@/components/icons";
+import { leaveRoomEmit } from "@/utils/socketClient";
+import { ChatMemberProps } from "@/types/chat";
 
-const ChatMember = () => {
+const ChatMember = ({ members, roomId }: ChatMemberProps) => {
+  const router = useRouter();
+
   return (
     <ChatMemberContainer>
       <Header title="Member" />
@@ -17,6 +24,18 @@ const ChatMember = () => {
           <MemberNickname>Test</MemberNickname>
         </MemberInfo>
       </ChatMemberList>
+      <ChatButtonContainer>
+        <Button
+          type="button"
+          bgColor="secondary"
+          onClick={() => {
+            leaveRoomEmit(11, roomId);
+            router.push("/");
+          }}
+        >
+          <ExitIcon>{getIcons("exit", 32)}나가기</ExitIcon>
+        </Button>
+      </ChatButtonContainer>
     </ChatMemberContainer>
   );
 };
@@ -24,7 +43,7 @@ const ChatMember = () => {
 export default ChatMember;
 
 const ChatMemberContainer = tw.div`
-  shrink-0 w-72 bg-zinc-900 rounded-l
+  relative shrink-0 w-80 bg-zinc-900 rounded-l
 `;
 
 const ChatMemberList = tw.div`
@@ -41,4 +60,12 @@ const MemberProfilePic = tw(Image)`
 
 const MemberNickname = tw.span`
   flex items-center h-16 ml-4
+`;
+
+const ExitIcon = tw.div`
+  flex items-center justify-center
+`;
+
+const ChatButtonContainer = tw.div`
+  absolute w-full px-4 bottom-0
 `;
