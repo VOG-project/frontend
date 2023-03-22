4853bf121e142295d1f3b8a59d59f814d92fbbc2
origin1508 <origin1508@gmail.com>
2023-03-22 21:57:55 +0900

    fix: ChatEdit 수정

--- components/Chat/ChatEdit/ChatEdit.tsx
+++ components/Chat/ChatEdit/ChatEdit.tsx
@@ -1,7 +1,8 @@
 import tw from "twin.macro";
-import useChatForm from "@/hooks/useChatForm";
+import useChatEditForm, { ChatEditValue } from "@/hooks/useChatEditForm";
 import Input from "@/components/common/Input";
 import Modal from "@/components/common/Modal";
+import Button from "@/components/common/Button";
 import { createChatRoomRequest } from "@/apis/chat";
 
 interface ChatEditProps {
@@ -10,11 +11,10 @@ interface ChatEditProps {
 }
 
 const ChatEdit = ({ isOpen, handleModalClose }: ChatEditProps) => {
-  const { register, handleSubmit } = useChatForm();
-  const handleChatRoomCreate = async (data) => {
+  const { register, handleSubmit } = useChatEditForm();
+  const handleChatRoomCreate = async (data: ChatEditValue) => {
     const { title, maximumMember } = data;
-    console.log(title, maximumMember);
-    const res = await createChatRoomRequest(11, chat.title, chat.maximumMember);
+    const res = await createChatRoomRequest(11, title, maximumMember);
 
     console.log(res);
   };
@@ -22,10 +22,11 @@ const ChatEdit = ({ isOpen, handleModalClose }: ChatEditProps) => {
     <Modal
       title="방생성"
       isOpen={isOpen}
+      hasFooter={false}
       handleClose={handleModalClose}
       handleConfirm={handleModalClose}
     >
-      <ChatEditForm>
+      <ChatEditForm onSubmit={handleSubmit(handleChatRoomCreate)}>
         <Input
           register={register("title")}
           width={18}
@@ -33,11 +34,25 @@ const ChatEdit = ({ isOpen, handleModalClose }: ChatEditProps) => {
           placeholder="제목을 입력하세요"
         />
         <Input
+          type="number"
           register={register("maximumMember")}
           width={18}
           bgColor="gray"
           placeholder="최대 인원을 입력해주세요"
         />
+        <ChatEditButtonContainer>
+          <Button
+            type="button"
+            bgColor="secondary"
+            width={4}
+            onClick={handleModalClose}
+          >
+            취소
+          </Button>
+          <Button type="submit" bgColor="primary" width={4}>
+            생성
+          </Button>
+        </ChatEditButtonContainer>
       </ChatEditForm>
     </Modal>
   );
@@ -47,4 +62,8 @@ export default ChatEdit;
 
 const ChatEditForm = tw.form`
   flex flex-col w-auto
+`;
+
+const ChatEditButtonContainer = tw.div`
+  flex justify-end px-8 space-x-4
 `;
