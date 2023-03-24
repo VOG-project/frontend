48ed62c492a5c89cd7dc974874d17b44af3ab97d
origin1508 <origin1508@gmail.com>
2023-03-24 22:07:07 +0900

    fix: Content 스타일 변경

--- components/Community/Contents/Contents.tsx
+++ components/Community/Contents/Contents.tsx
@@ -2,6 +2,7 @@ import { useRouter } from "next/router";
 import tw from "twin.macro";
 import { getIcons } from "@/components/icons";
 import { Content } from "@/types/community";
+import timeDifference from "@/utils/timeDifference";
 
 interface ContentsProps {
   contents: Content[];
@@ -10,25 +11,38 @@ interface ContentsProps {
 
 const Contents = ({ contents, category }: ContentsProps) => {
   const router = useRouter();
-  const handleContentClick = () => {
+  const handleContentClick = (postId: number) => {
     router.push({
       pathname: "/community/[id]",
-      query: { id: "123", category: category },
+      query: { id: postId, category: category },
     });
   };
   return (
     <ContentsContainer>
       {contents.map((content) => {
         return (
-          <ContentContainer key={content.id} onClick={handleContentClick}>
-            <ContentGame>{content.gameCategory}</ContentGame>
+          <ContentContainer
+            key={content.id}
+            onClick={() => handleContentClick(content.id)}
+          >
+            <ContentLikeCount>
+              {getIcons("thumb", 18)}
+              {content.likeCount}
+            </ContentLikeCount>
+            <ContentGame>리그오브레전드</ContentGame>
             <ContentTitle>
               {content.title}
               <CommentCount>[0]</CommentCount>
             </ContentTitle>
-            <ContentAuthor>{content.writerId}</ContentAuthor>
-            <ContentHit>{getIcons("eye", 18)}29</ContentHit>
-            <ContentTime>{getIcons("time", 18)}1시간전</ContentTime>
+            <ContentAuthor>{content.id}</ContentAuthor>
+            <ContentHit>
+              {getIcons("thumb", 18)}
+              {content.likeCount}
+            </ContentHit>
+            <ContentTime>
+              {getIcons("time", 18)}
+              {timeDifference(content.createdAt)}
+            </ContentTime>
           </ContentContainer>
         );
       })}
@@ -43,27 +57,31 @@ const ContentsContainer = tw.section`
 `;
 
 const ContentContainer = tw.div`
-  flex items-center w-full h-20 px-4 text-center
+  flex items-center w-full h-12 px-4 text-center
 `;
 
-const ContentGame = tw.div`
+const ContentGame = tw.span`
   w-1/12
 `;
 
-const ContentTitle = tw.div`
-  w-8/12 text-left
+const ContentTitle = tw.span`
+  grow text-left
 `;
 
 const CommentCount = tw.span`
   text-primary 
 `;
 
-const ContentAuthor = tw.div`
+const ContentAuthor = tw.span`
   w-1/12
 `;
 
-const ContentHit = tw.div`
-  flex justify-center items-center w-1/12
+const ContentLikeCount = tw.span`
+  flex w-[5%] justify-center items-center px-2
+`;
+
+const ContentHit = tw.span`
+  flex justify-center items-center px-2
 `;
 
 const ContentTime = tw.div`
