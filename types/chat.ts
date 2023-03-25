19fd88f764aec4540cc9e7a18b599ca7438a7371
origin1508 <origin1508@gmail.com>
2023-03-25 21:41:15 +0900

    fix: chat관련 타입들 정의

--- types/chat.ts
+++ types/chat.ts
@@ -1,12 +1,40 @@
 import { ParsedUrlQuery } from "querystring";
+import { SetterOrUpdater } from "recoil";
 
 export interface ChatQuery extends ParsedUrlQuery {
   id: string;
 }
 
-export interface ChatMemberProps {
-  members: string | null;
+export interface ChatState {
+  chatParticipant: { userId: number; socketId: string; nickname: string }[];
+  messages: {
+    content: string;
+    nickname: string;
+    roomId: string;
+    isSender: boolean;
+  }[];
+  title: string;
   roomId: string;
+}
+
+export interface ChatMemberProps {
+  members: { userId: number; socketId: string; nickname: string }[];
+  handleChatRoomLeave: () => void;
+}
+
+export interface ChatEditProps {
+  isOpen: boolean;
+  handleModalClose: () => void;
+  handleChatRoomCreate: (data: ChatEditValue) => Promise<void>;
+}
+
+export interface ChatMessageProps {
+  messages: {
+    content: string;
+    nickname: string;
+    roomId: string;
+    isSender: boolean;
+  }[];
 }
 
 export interface ChatRoom {
@@ -21,6 +49,7 @@ export interface ChatRoom {
 
 export interface RoomListProps {
   roomList: ChatRoom[];
+  handleRoomClick: (roomId: string) => void;
 }
 
 export interface ChatProps {
@@ -29,4 +58,16 @@ export interface ChatProps {
     result: ChatRoom[];
     chatRoomCount: number;
   };
+}
+
+export interface ChatSocketProps {
+  isChatRoom: boolean;
+  setChat: SetterOrUpdater<ChatState>;
+  socketConnect: () => void;
+  handleChatRoomLeave: () => void;
+}
+
+export interface ChatEditValue {
+  title: string;
+  maximumMember: number;
 }
