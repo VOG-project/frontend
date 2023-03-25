104a2e1b6aa3b285c24b336e5d21de09a17e4730
origin1508 <origin1508@gmail.com>
2023-03-25 22:51:51 +0900

    fix: 에러메시지 수정

--- components/Login/Login.tsx
+++ components/Login/Login.tsx
@@ -57,7 +57,7 @@ const Login = () => {
               bgColor="gray"
             />
             {watchEmail && emailError && (
-              <ErrorMessage>유효한 이메일을 입력해주세요.</ErrorMessage>
+              <ErrorMessage>유효한 이메일을 입력하세요.</ErrorMessage>
             )}
           </LoginInputContainer>
           <LoginInputContainer>
