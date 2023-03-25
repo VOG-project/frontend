4e7acb7c23b28c33f2c522b022dbb26d0283207e
origin1508 <origin1508@gmail.com>
2023-03-25 22:32:16 +0900

    fix: reactStricMode false

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withTwin = require("./withTwin");
 /** @type {import('next').NextConfig} */
 
 const nextConfig = withTwin({
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: true,
 });
 
