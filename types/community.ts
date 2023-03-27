2d0044f15cfdb7a52fe995e69fbbbec08899a23f
origin1508 <origin1508@gmail.com>
2023-03-28 01:52:15 +0900

    fix: Comment 관련 타입 추가

--- types/community.ts
+++ types/community.ts
@@ -13,6 +13,36 @@ export interface Content {
   };
 }
 
+export interface ContentDetail {
+  content: string;
+  createdAt: string;
+  id: number;
+  likeCount: number;
+  postCategory: string;
+  title: string;
+  updatedAt: string;
+  comments: Comment[];
+  user: {
+    id: number;
+    nickname: string;
+  };
+}
+
+export interface Comment {
+  content: string;
+  createdAt: string;
+  group: number;
+  id: number;
+  sequence: number;
+  updatedAt: string;
+  userId: number;
+  user: {
+    id: number;
+    nickname: string;
+  };
+  reply: Comment[];
+}
+
 export interface CommunityProps {
   data: {
     success: boolean;
@@ -21,28 +51,22 @@ export interface CommunityProps {
   postCount: number;
 }
 
+export interface PostProps {
+  content?: ContentDetail;
+}
+
+export interface CommentsProps {
+  comments: Comment[];
+}
+
+export interface CommentProps {
+  author: string;
+  createdAt: string;
+  content: string;
+  reply: Comment[];
+}
+
 export interface CommunityQuery extends ParsedUrlQuery {
   category: string;
   id: string;
-}
-
-export interface ContentDetail {
-  content: string;
-  gameCategory: string;
-  id: number;
-  likeCount: number;
-  title: string;
-  updatedAt: string;
-  user: {
-    updatedAt: string;
-    id: number;
-    email: string;
-    nickname: string;
-    sex: number;
-    wirterId: number;
-  };
-}
-
-export interface PostProps {
-  content?: ContentDetail;
 }
