ddb2ee1c421280c12c2324e66e94e8a46d1a15e1
origin1508 <origin1508@gmail.com>
2023-03-29 00:16:04 +0900

    feat: Textarea

---
+++ components/common/Textarea/index.tsx
@@ -0,0 +1,41 @@
+import { RefObject, ChangeEvent, KeyboardEvent } from "react";
+import tw from "twin.macro";
+
+interface TextareaProps {
+  placeholder: string;
+  textareaRef: RefObject<HTMLTextAreaElement>;
+  buttonRef: RefObject<HTMLButtonElement>;
+}
+
+const Textarea = ({ placeholder, textareaRef, buttonRef }: TextareaProps) => {
+  const handleTextAreaChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    e.target.style.height = "auto";
+    const scrollHeight = e.target.scrollHeight;
+    e.target.style.height = scrollHeight + "px";
+  };
+
+  const handleTextAreaKeyDown = (e: KeyboardEvent<HTMLTextAreaElement>) => {
+    if (!e.shiftKey && e.key === "Enter") {
+      e.preventDefault();
+      buttonRef.current?.click();
+    }
+    return;
+  };
+
+  return (
+    <StyledTextarea
+      placeholder={placeholder}
+      rows={1}
+      ref={textareaRef}
+      onChange={handleTextAreaChange}
+      onKeyDown={handleTextAreaKeyDown}
+    ></StyledTextarea>
+  );
+};
+
+export default Textarea;
+
+const StyledTextarea = tw.textarea`
+  shrink grow px-4 py-2 w-full max-h-96 bg-stone-800 resize-none 
+  focus:(outline-none placeholder-transparent)
+`;
