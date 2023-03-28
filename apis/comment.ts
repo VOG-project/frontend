f9ecf6481eb63625282f971a1668d712ca2ac710
origin1508 <origin1508@gmail.com>
2023-03-29 00:14:49 +0900

    feat: comment 관련 api

---
+++ apis/comment.ts
@@ -0,0 +1,56 @@
+import customAxios from "@/utils/customAxios";
+
+const getCommentsRequest = async (postId: number) => {
+  const res = await customAxios().get(`/comments/${postId}`);
+
+  return res.data;
+};
+
+const createCommentRequest = async (
+  userId: number,
+  postId: number,
+  content: string,
+  group: number,
+  sequence: number
+) => {
+  const res = await customAxios().post("/comments", {
+    userId,
+    postId,
+    content,
+    group,
+    sequence,
+  });
+
+  return res.data;
+};
+
+const deleteCommentRequest = async (
+  group: number,
+  sequence: number,
+  postId: number
+) => {
+  const res = await customAxios().delete("/comments", {
+    params: {
+      group,
+      sequence,
+      postId,
+    },
+  });
+
+  return res.data;
+};
+
+const editCommentRequest = async (commentId: number, content: string) => {
+  const res = await customAxios().patch(`/comments/${commentId}`, {
+    content: content,
+  });
+
+  return res.data;
+};
+
+export {
+  getCommentsRequest,
+  createCommentRequest,
+  deleteCommentRequest,
+  editCommentRequest,
+};
