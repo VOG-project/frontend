2787189e43c66d93bfe1e3d47c4f5ad463d03c7c
origin1508 <origin1508@gmail.com>
2023-03-29 00:14:41 +0900

    fix: images domain 추가

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const withTwin = require("./withTwin");
 const nextConfig = withTwin({
   reactStrictMode: false,
   swcMinify: true,
+  images: {
+    domains: ["vog-image-storage.s3.ap-northeast-2.amazonaws.com"],
+  },
 });
 
 module.exports = nextConfig;
