ab50a050189248688d8ac3a9dc7fe5b2ec5802cc
origin1508 <origin1508@gmail.com>
2023-03-30 22:56:54 +0900

    fix: userFirendState내에 friend 관련 api 함수를 정의

--- hooks/useFriendState.ts
+++ hooks/useFriendState.ts
@@ -1,8 +1,15 @@
 import { useRecoilState } from "recoil";
 import { friendState } from "@/recoil/atoms/friendState";
+import useToast from "./useToast";
+import {
+  addFriendRequest,
+  removeFriendRequest,
+  getFriendsRequest,
+} from "@/apis/friend";
 
 const useFriendState = () => {
   const [friend, setFriend] = useRecoilState(friendState);
+  const { toast } = useToast();
 
   const handleFriendToggle = () => {
     setFriend((prev) => {
@@ -10,10 +17,62 @@ const useFriendState = () => {
     });
   };
 
+  const updateFriendList = async (userId: number) => {
+    const res = await getFriendsRequest(userId);
+
+    if (res.success) {
+      setFriend((prev) => {
+        return { ...prev, friends: res.result };
+      });
+    } else {
+      toast.alert(res.error);
+    }
+  };
+
+  const handleRemoveFriendClick = async (
+    userId: number | null,
+    targetId: number | null
+  ) => {
+    if (!userId) return;
+    if (!targetId) return;
+    const res = await removeFriendRequest(userId, targetId);
+
+    if (res.success) {
+      await updateFriendList(userId);
+    } else {
+      toast.alert(res.error);
+    }
+  };
+
+  const handleAddFriendClick = async (
+    userId: number | null,
+    targetId: number | null
+  ) => {
+    if (!userId) return;
+    if (!targetId) return;
+    const res = await addFriendRequest(userId, targetId);
+
+    if (res.success) {
+      await updateFriendList(userId);
+    } else {
+      toast.alert(res.error);
+    }
+  };
+
   const isShow = friend.isShow;
   const friends = friend.friends;
+  const friendIds = friends.map((friend) => friend.following.id);
 
-  return { friends, isShow, setFriend, handleFriendToggle };
+  return {
+    friends,
+    friendIds,
+    isShow,
+    setFriend,
+    updateFriendList,
+    handleFriendToggle,
+    handleRemoveFriendClick,
+    handleAddFriendClick,
+  };
 };
 
 export default useFriendState;
