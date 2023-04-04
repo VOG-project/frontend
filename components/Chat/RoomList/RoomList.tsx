d3df9a58f0ea75498d05416afb25c8f7b87eb8f0
origin1508 <origin1508@gmail.com>
2023-04-05 00:06:27 +0900

    fix: 스타일 변경 진행중

--- components/Chat/RoomList/RoomList.tsx
+++ components/Chat/RoomList/RoomList.tsx
@@ -7,15 +7,13 @@ const RoomList = ({ roomList, handleRoomClick }: RoomListProps) => {
       {roomList.map((room) => {
         return (
           <Room key={room.roomId} onClick={() => handleRoomClick(room.roomId)}>
-            <RoomInfoContainer>
-              <RoomInfo>
-                <RoomGame>리그 오브 레전드</RoomGame>
-                <RoomTitle>{room.title}</RoomTitle>
-                <RoomMemberCount>
-                  {room.currentMember} / {room.maximumMember}
-                </RoomMemberCount>
-              </RoomInfo>
-            </RoomInfoContainer>
+            <RoomInfo>
+              {/* <RoomGame>발로란트</RoomGame> */}
+              <RoomTitle>{room.title}</RoomTitle>
+              <RoomMemberCount>
+                {room.currentMember} / {room.maximumMember}
+              </RoomMemberCount>
+            </RoomInfo>
           </Room>
         );
       })}
@@ -26,24 +24,20 @@ const RoomList = ({ roomList, handleRoomClick }: RoomListProps) => {
 export default RoomList;
 
 const RoomListContainer = tw.ul`
-  w-full
+  grid grid-cols-2 gap-8 w-full
 `;
 
 const Room = tw.li`
-  relative inline-block w-[20%] pt-[20%] text-center cursor-pointer
-`;
-
-const RoomInfoContainer = tw.div`
-  absolute flex flex-col top-0 left-0 right-0 bottom-0 p-4
+  inline-block w-full text-center cursor-pointer
 `;
 
 const RoomInfo = tw.div`
-  flex flex-col w-full h-full shadow bg-white/10
+  flex flex-col w-full h-full shadow bg-white/10 rounded-2xl
 `;
 
-const RoomGame = tw.div`
-  border-b border-neutral-700
-`;
+// const RoomGame = tw.div`
+//   border-b border-neutral-700
+// `;
 
 const RoomTitle = tw.div`
   pt-4 h-full text-3xl
