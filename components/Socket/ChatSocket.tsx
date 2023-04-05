bc4084542a03e79fd12bfdac7408b0f452b5aa60
origin1508 <origin1508@gmail.com>
2023-04-05 22:34:13 +0900

    fix: 1:n 음성채팅 구현

--- components/Socket/ChatSocket.tsx
+++ components/Socket/ChatSocket.tsx
@@ -1,26 +1,55 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import tw, { styled } from "twin.macro";
-import useWebRTC from "@/hooks/useWebRTC";
+// import useWebRTC from "@/hooks/useWebRTC";
 import Button from "../common/Button";
+import Audio from "../common/Audio/Audio";
 import { socketClient } from "@/utils/socketClient";
 import { getIcons } from "../icons";
 import { ChatSocketProps } from "@/types/chat";
 import { ChatState } from "@/types/chat";
 
+const RTC_CONFIG = {
+  iceServers: [
+    {
+      urls: [
+        "stun:stun.l.google.com:19302",
+        "stun:stun1.l.google.com:19302",
+        "stun:stun2.l.google.com:19302",
+        "stun:stun3.l.google.com:19302",
+        "stun:stun4.l.google.com:19302",
+      ],
+    },
+  ],
+};
+
+const CONSTRAINTS = {
+  audio: true,
+};
+
 const ChatSocket = ({
   isChatRoom,
+  chat,
   setChat,
   socketConnect,
   handleChatRoomLeave,
 }: ChatSocketProps) => {
-  const peerStreamRef = useRef<HTMLAudioElement>(null);
-  const {
-    createPeerConnection,
-    sendOffer,
-    getOffer,
-    getAnswer,
-    getIceCandidate,
-  } = useWebRTC();
+  // const {
+  //   createPeerConnection,
+  //   sendOffer,
+  //   getOffer,
+  //   getAnswer,
+  //   getIceCandidate,
+  // } = useWebRTC();
+
+  const peerConnections: { [key: string]: RTCPeerConnection } = {};
+  const handleTrack = (e: RTCTrackEvent, socketId: string) => {
+    setChat((prev) => {
+      return {
+        ...prev,
+        streams: { ...prev.streams, [socketId]: e.streams[0] },
+      };
+    });
+  };
 
   useEffect(() => {
     socketConnect();
@@ -33,8 +62,32 @@ const ChatSocket = ({
     });
 
     socketClient.on("welcome", async (socketId) => {
-      await createPeerConnection(socketId);
-      sendOffer(socketId);
+      // await createPeerConnection(socketId);
+      // await sendOffer(socketId);
+      const peerConnection = new RTCPeerConnection(RTC_CONFIG);
+
+      const myStream = await navigator.mediaDevices.getUserMedia(CONSTRAINTS);
+      myStream
+        .getTracks()
+        .forEach((track) => peerConnection.addTrack(track, myStream));
+
+      peerConnection.onicecandidate = (e) => {
+        if (e.candidate) {
+          socketClient.emit("iceCandidate", {
+            targetId: socketId,
+            iceCandidate: e.candidate,
+          });
+        }
+      };
+
+      peerConnection.ontrack = (e) => {
+        handleTrack(e, socketId);
+      };
+
+      peerConnections[socketId] = peerConnection;
+      const offer = await peerConnection.createOffer({});
+      await peerConnection.setLocalDescription(offer);
+      socketClient.emit("offer", { targetId: socketId, offer });
     });
 
     socketClient.on("inputChat", (data) => {
@@ -59,18 +112,49 @@ const ChatSocket = ({
 
     socketClient.on("offer", async (data) => {
       const { socketId, offer } = data;
-      await createPeerConnection(socketId);
-      getOffer(socketId, offer);
+      // await createPeerConnection(socketId);
+      // await getOffer(socketId, offer);
+      const peerConnection = new RTCPeerConnection();
+      const myStream = await navigator.mediaDevices.getUserMedia(CONSTRAINTS);
+      myStream
+        .getTracks()
+        .forEach((track) => peerConnection.addTrack(track, myStream));
+
+      peerConnection.onicecandidate = (e) => {
+        if (e.candidate) {
+          socketClient.emit("iceCandidate", {
+            targetId: socketId,
+            iceCandidate: e.candidate,
+          });
+        }
+      };
+
+      peerConnection.ontrack = (e) => {
+        handleTrack(e, socketId);
+      };
+
+      peerConnections[socketId] = peerConnection;
+
+      await peerConnection.setRemoteDescription(offer);
+      const answer = await peerConnection.createAnswer({});
+      await peerConnection.setLocalDescription(answer);
+      socketClient.emit("answer", { targetId: socketId, answer });
     });
 
-    socketClient.on("answer", (data) => {
+    socketClient.on("answer", async (data) => {
       const { socketId, answer } = data;
-      getAnswer(socketId, answer);
+      // await getAnswer(socketId, answer);
+      const peerConnection = peerConnections[socketId];
+      await peerConnection.setRemoteDescription(answer);
     });
 
-    socketClient.on("iceCandidate", (data) => {
+    socketClient.on("iceCandidate", async (data) => {
       const { socketId, iceCandidate } = data;
-      getIceCandidate(socketId, iceCandidate);
+      // await getIceCandidate(socketId, iceCandidate);
+      const peerConnection = peerConnections[socketId];
+      if (peerConnections[socketId]) {
+        await peerConnection.addIceCandidate(iceCandidate);
+      }
     });
 
     return () => {
@@ -82,8 +166,16 @@ const ChatSocket = ({
     <ChatSocketContainer isChatRoom={isChatRoom}>
       <Button width={6} bgColor="secondary" onClick={handleChatRoomLeave}>
         <LeaveChatButtonIcon>{getIcons("exit", 24)}나가기</LeaveChatButtonIcon>
-        <audio ref={peerStreamRef} autoPlay></audio>
       </Button>
+      {chat.chatParticipant
+        .reduce((acc: string[], cur) => {
+          const socketId = cur.socketId;
+          return [...acc, socketId];
+        }, [])
+        .map((socketId) => {
+          const stream = chat.streams[socketId];
+          return <Audio key={socketId} stream={stream}></Audio>;
+        })}
     </ChatSocketContainer>
   );
 };
