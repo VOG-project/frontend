c32636480ad8cc2ebd31db3bd575257df61fb033
origin1508 <origin1508@gmail.com>
2023-04-07 23:22:52 +0900

    fix: typeError 수정,

--- components/Socket/ChatSocket.tsx
+++ components/Socket/ChatSocket.tsx
@@ -122,28 +122,20 @@ const ChatSocket = ({
       peerConnection.setLocalDescription(answer);
       console.log("send answer: ", answer);
       socketClient.emit("answer", { targetId: socketId, answer: answer });
-
-      if (iceCandidateRef.current[socketId]) {
-        iceCandidateRef.current[socketId].map((iceCandidate) => {
-          peerConnection.addIceCandidate(iceCandidate);
-          iceCandidateRef.current[socketId] = [];
-        });
-      }
     });
 
     socketClient.on("answer", (data) => {
       const { socketId, answer } = data;
-      console.log("getAnswer", socketId, answer);
-      const peerConnection = peerConnectionsRef.current[socketId];
-      console.log(peerConnection);
-      peerConnection.setRemoteDescription(answer);
-
       if (iceCandidateRef.current[socketId]) {
         iceCandidateRef.current[socketId].map((iceCandidate) => {
           peerConnection.addIceCandidate(iceCandidate);
           iceCandidateRef.current[socketId] = [];
         });
       }
+      console.log("getAnswer", socketId, answer);
+      const peerConnection = peerConnectionsRef.current[socketId];
+      console.log(peerConnection);
+      peerConnection.setRemoteDescription(answer);
     });
 
     socketClient.on("iceCandidate", (data) => {
@@ -152,7 +144,7 @@ const ChatSocket = ({
       const peerConnection = peerConnectionsRef.current[socketId];
       console.log("getCandidate", socketId, iceCandidate, peerConnection);
       if (peerConnection.remoteDescription == null) {
-        if (iceCandidateRef.current[socketId].length === 0) {
+        if (!iceCandidateRef.current[socketId]) {
           iceCandidateRef.current[socketId] = [iceCandidate];
         } else {
           iceCandidateRef.current[socketId].push(iceCandidate);
