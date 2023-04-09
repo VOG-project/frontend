eff43e5cd3d83524563f5e2cb900a3b0176e6e33
origin1508 <origin1508@gmail.com>
2023-04-10 02:22:38 +0900

    fix: Socket refactoring

--- components/Socket/Socket.tsx
+++ components/Socket/Socket.tsx
@@ -1,25 +1,31 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import tw, { styled } from "twin.macro";
-import ChatSocket from "./ChatSocket";
+import useModal from "@/hooks/useModal";
 import useChatState from "@/hooks/useChatState";
 import useUserState from "@/hooks/useUserState";
+import useUserProfileState from "@/hooks/useUserProfileState";
+import ChatSocket from "./ChatSocket";
+import DeviceSettingModal from "./DeviceSettingModal";
 import { socketClient } from "@/utils/socketClient";
 import { enterRoomEmit, leaveRoomEmit } from "@/utils/socketClient";
+import useMediaDevice from "@/hooks/useMediaDevice";
 
 const Socket = () => {
   const [isChatRoom, setIsChatRoom] = useState(false);
   const router = useRouter();
+  const { isOpen, handleModalOpen, handleModalClose } = useModal();
   const { chat, setChat, resetChat } = useChatState();
   const { userId, user } = useUserState();
+  const { handleUserProfileOpen } = useUserProfileState();
+  const { peerConnectionsRef } = useMediaDevice();
+
   const roomId = chat.roomId;
 
   useEffect(() => {
     if (router.query.id === chat.roomId) {
-      console.log("true");
       setIsChatRoom(true);
     } else {
-      console.log("false");
       setIsChatRoom(false);
     }
   }, [router]);
@@ -38,6 +44,12 @@ const Socket = () => {
     }
   };
 
+  const handleTitleClick = () => {
+    if (chat.roomId) {
+      router.push(`/chat/${roomId}`);
+    }
+  };
+
   const socketConnect = () => {
     if (!userId) return;
 
@@ -48,6 +60,11 @@ const Socket = () => {
   const handleChatRoomLeave = () => {
     if (!userId) return;
 
+    if (peerConnectionsRef.current) {
+      Object.values(peerConnectionsRef.current).forEach((peerConnection) =>
+        peerConnection.close()
+      );
+    }
     leaveRoomEmit(userId, roomId);
     resetChat();
   };
@@ -57,11 +74,21 @@ const Socket = () => {
       {roomId && (
         <ChatSocket
           chat={chat}
+          peerConnectionsRef={peerConnectionsRef}
           setChat={setChat}
           socketConnect={socketConnect}
           handleChatRoomLeave={handleChatRoomLeave}
+          handleTitleClick={handleTitleClick}
+          handleUserProfileOpen={handleUserProfileOpen}
+          handleModalOpen={handleModalOpen}
         />
       )}
+      <DeviceSettingModal
+        peerConnectionsRef={peerConnectionsRef}
+        isOpen={isOpen}
+        handleClose={handleModalClose}
+        handleConfirm={handleModalClose}
+      />
     </SocketContainer>
   );
 };
@@ -70,7 +97,7 @@ export default Socket;
 
 const SocketContainer = styled.div<{ isChatRoom: boolean }>(
   ({ isChatRoom }) => [
-    tw`fixed flex items-center justify-center left-1/2 bottom-0 -translate-x-1/2 pl-64 w-full max-w-[120rem] z-[100]`,
+    tw`fixed flex items-center justify-center bottom-0 pl-64 w-full max-w-[120rem] z-10`,
     isChatRoom && tw`translate-y-64`,
   ]
 );
