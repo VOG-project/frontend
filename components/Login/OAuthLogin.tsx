a3468e30f0744e80ad7334a1cb4ef776986b55f6
origin1508 <origin1508@gmail.com>
2023-04-12 21:50:39 +0900

    fix: Link import

--- components/Login/OAuthLogin.tsx
+++ components/Login/OAuthLogin.tsx
@@ -1,19 +1,21 @@
 import tw from "twin.macro";
 import Button from "../common/Button";
+import Link from "next/link";
 
 const OAuthLogin = () => {
   const NAVER_CLIENT_ID = process.env.NEXT_PUBLIC_NAVER_CLIENT_ID;
   const NAVER_CLIENT_STATE = process.env.NEXT_PUBLIC_NAVER_CLIENT_STATE;
+
   return (
     <OAuthContainer>
       <Button bgColor="secondary" width={18}>
         Google
       </Button>
-      <a
-        href={`https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=${NAVER_CLIENT_STATE}&redirect_uri=https://talkgg.online/auth/login/naver`}
+      <Link
+        href={`https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=${NAVER_CLIENT_STATE}&redirect_uri=http://localhost:3000/auth/login/naver`}
       >
         Naver
-      </a>
+      </Link>
     </OAuthContainer>
   );
 };
