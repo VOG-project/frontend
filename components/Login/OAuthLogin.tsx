6ca5668f3f3341f49cc4629712fceca391a41102
origin1508 <origin1508@gmail.com>
2023-04-12 22:33:35 +0900

    fix: 네이버 로그인 버튼 스타일 수정

--- components/Login/OAuthLogin.tsx
+++ components/Login/OAuthLogin.tsx
@@ -8,18 +8,34 @@ const OAuthLogin = () => {
 
   return (
     <OAuthContainer>
+      <NaverLogin
+        href={`https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=${NAVER_CLIENT_STATE}&redirect_uri=http://localhost:3000/auth/login/naver`}
+      >
+        <NaverIcon></NaverIcon>
+        <NaverText>Log in with Naver</NaverText>
+      </NaverLogin>
+
       <Button bgColor="secondary" width={18}>
         Google
       </Button>
-      <Link
-        href={`https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=${NAVER_CLIENT_STATE}&redirect_uri=http://localhost:3000/auth/login/naver`}
-      >
-        Naver
-      </Link>
     </OAuthContainer>
   );
 };
 
 export default OAuthLogin;
 
-const OAuthContainer = tw.div`flex flex-col items-center px-10`;
+const OAuthContainer = tw.div`
+  flex w-full flex-col items-center m-auto
+`;
+
+const NaverLogin = tw(Link)`
+  flex items-center justify-center w-3/6 h-12 rounded bg-[#03C75A]
+`;
+
+const NaverIcon = tw.div`
+  shrink-0 w-12 h-12 bg-[url("/image/naver.png")] bg-cover bg-center
+`;
+
+const NaverText = tw.div`
+  flex items-center justify-center w-32 text-center
+`;
