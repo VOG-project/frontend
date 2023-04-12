1596a91597ece58eadd63c785b67872a72d4cb1d
origin1508 <origin1508@gmail.com>
2023-04-12 18:09:15 +0900

    fix: naver클릭시 oauth 인증 요청

--- components/Login/OAuthLogin.tsx
+++ components/Login/OAuthLogin.tsx
@@ -2,14 +2,18 @@ import tw from "twin.macro";
 import Button from "../common/Button";
 
 const OAuthLogin = () => {
+  const NAVER_CLIENT_ID = process.env.NEXT_PUBLIC_NAVER_CLIENT_ID;
+  const NAVER_CLIENT_SECRET = process.env.NEXT_PUBLIC_NAVER_CLIENT_SECRET;
   return (
     <OAuthContainer>
       <Button bgColor="secondary" width={18}>
         Google
       </Button>
-      <Button bgColor="secondary" width={18}>
+      <a
+        href={`https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=${NAVER_CLIENT_SECRET}&redirect_uri=http://localhost:3000/auth/login/naver`}
+      >
         Naver
-      </Button>
+      </a>
     </OAuthContainer>
   );
 };
