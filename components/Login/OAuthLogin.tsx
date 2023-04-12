8fa4fdb9a94852231b69b91a92406563deb68f55
origin1508 <origin1508@gmail.com>
2023-04-12 20:20:05 +0900

    fix: redirect_uri 수정

--- components/Login/OAuthLogin.tsx
+++ components/Login/OAuthLogin.tsx
@@ -3,14 +3,14 @@ import Button from "../common/Button";
 
 const OAuthLogin = () => {
   const NAVER_CLIENT_ID = process.env.NEXT_PUBLIC_NAVER_CLIENT_ID;
-  const NAVER_CLIENT_SECRET = process.env.NEXT_PUBLIC_NAVER_CLIENT_SECRET;
+  const NAVER_CLIENT_STATE = process.env.NEXT_PUBLIC_NAVER_CLIENT_STATE;
   return (
     <OAuthContainer>
       <Button bgColor="secondary" width={18}>
         Google
       </Button>
       <a
-        href={`https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=${NAVER_CLIENT_SECRET}&redirect_uri=http://localhost:3000/auth/login/naver`}
+        href={`https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=${NAVER_CLIENT_STATE}&redirect_uri=https://talkgg.online/auth/login/naver`}
       >
         Naver
       </a>
