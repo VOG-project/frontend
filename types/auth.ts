f4b09e2748f2e0f9b526ef6fadb7d06a17c4f01b
origin1508 <origin1508@gmail.com>
2023-04-12 23:38:45 +0900

    fix: NaverLoginQuery 추가

--- types/auth.ts
+++ types/auth.ts
@@ -1,7 +1,14 @@
+import { ParsedUrlQuery } from "querystring";
+
 export interface SignUpValue {
   email: string;
   nickname: string;
   password: string;
   confirmPassword: string;
   gender: string;
+}
+
+export interface NaverLoginQuery extends ParsedUrlQuery {
+  code: string;
+  state: string;
 }
