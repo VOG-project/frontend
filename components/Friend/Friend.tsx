cdc6c90cbd98e8d12aaec3308f96e770bf1905a6
origin1508 <origin1508@gmail.com>
2023-04-13 22:25:03 +0900

    fix: context 메뉴 기능 구현

--- components/Friend/Friend.tsx
+++ components/Friend/Friend.tsx
@@ -1,12 +1,48 @@
-import tw, { styled } from "twin.macro";
+import { MouseEvent, useState } from "react";
+import tw, { styled, css } from "twin.macro";
 import useFriendState from "@/hooks/useFriendState";
+import useUserProfileState from "@/hooks/useUserProfileState";
 import Header from "../common/Header";
 import UserCard from "../common/UserCard";
 import { getIcons } from "../icons";
+import { CONTEXT_MENU_WIDTH } from "@/constants/friend";
 
 const Friend = () => {
   const { friends, isShow, handleFriendToggle, handleRemoveFriendClick } =
     useFriendState();
+  const { handleUserProfileOpen } = useUserProfileState();
+  const [contextMenuState, setContextMenuState] = useState({
+    isShow: false,
+    x: 0,
+    y: 0,
+    reverse: false,
+  });
+
+  const handleRightClick = (e: MouseEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    const maxWidth = window.innerWidth;
+    if (e.pageX + CONTEXT_MENU_WIDTH > maxWidth) {
+      setContextMenuState((prev) => {
+        return {
+          isShow: !prev.isShow,
+          x: maxWidth - e.pageX,
+          y: e.pageY,
+          reverse: true,
+        };
+      });
+    } else {
+      setContextMenuState((prev) => {
+        return {
+          isShow: !prev.isShow,
+          x: e.pageX,
+          y: e.pageY,
+          reverse: false,
+        };
+      });
+    }
+  };
+
+  console.log(contextMenuState);
 
   return (
     <FriendContainer isShow={isShow}>
@@ -15,17 +51,36 @@ const Friend = () => {
         {friends.map((friend) => {
           return (
             <FriendProfile key={friend.following.id}>
-              <UserCard
-                nickname={friend.following.nickname}
-                profilePic={friend.following.profileUrl}
-              />
-              <RemoveFriendBtn
-                onClick={() =>
-                  handleRemoveFriendClick(friend.userId, friend.following.id)
-                }
-              >
-                {getIcons("removeFriend", 32)}
-              </RemoveFriendBtn>
+              <ContextWrapper onClick={(e) => handleRightClick(e)}>
+                {contextMenuState.isShow && (
+                  <ContextMenus
+                    x={contextMenuState.x}
+                    y={contextMenuState.y}
+                    reverse={contextMenuState.reverse}
+                  >
+                    <ContextMenu
+                      onClick={() => handleUserProfileOpen(friend.userId)}
+                    >
+                      프로필
+                    </ContextMenu>
+                    <ContextMenu>채팅방 초대</ContextMenu>
+                    <ContextMenu
+                      onClick={() =>
+                        handleRemoveFriendClick(
+                          friend.userId,
+                          friend.following.id
+                        )
+                      }
+                    >
+                      친구 삭제
+                    </ContextMenu>
+                  </ContextMenus>
+                )}
+                <UserCard
+                  nickname={friend.following.nickname}
+                  profilePic={friend.following.profileUrl}
+                />
+              </ContextWrapper>
             </FriendProfile>
           );
         })}
@@ -52,16 +107,32 @@ const FriendProfile = tw.li`
   hover:([& button]:block)
 `;
 
-const RemoveFriendBtn = tw.button`
-  absolute hidden top-3 right-6 w-10 h-10 text-center text-secondary rounded
-  hover:(bg-white/20)
-  [& svg]:(inline-block align-middle)
-`;
-
 const FriendAddBtn = tw.button`
   shrink-0 flex items-center justify-center w-full h-12 border border-primary
   hover:bg-white/30
 `;
 const FriendToggle = tw.button`
   absolute p-2 top-1/2 -left-8
+`;
+
+const ContextWrapper = tw.div``;
+
+const ContextMenus = styled.ul<{ x: number; y: number; reverse: boolean }>(
+  ({ x, y, reverse }) => [
+    tw`fixed w-24 divide-y bg-white text-black `,
+    reverse
+      ? css`
+          top: ${y}px;
+          right: ${x}px;
+        `
+      : css`
+          top: ${y}px;
+          left: ${x}px;
+        `,
+  ]
+);
+
+const ContextMenu = tw.li`
+  p-2 cursor-pointer
+  hover:(bg-primary text-white)
 `;
