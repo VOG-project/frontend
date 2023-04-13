ee41394229c182cd0a7f83e758bb3b465baa3902
origin1508 <origin1508@gmail.com>
2023-04-13 20:04:56 +0900

    fix: Loading spinner 수정

--- components/common/Loading/Loading.tsx
+++ components/common/Loading/Loading.tsx
@@ -1,19 +1,12 @@
 import tw from "twin.macro";
+import useLoadingState from "@/hooks/useLoadingState";
+import Circle from "./Circle";
 
 const Loading = () => {
-  return (
-    <LoadingContainer>
-      <Cycle></Cycle>
-    </LoadingContainer>
-  );
+  const { isLoading } = useLoadingState();
+  return <LoadingContainer>{isLoading && <Circle />}</LoadingContainer>;
 };
 
 export default Loading;
 
-const LoadingContainer = tw.div`
-  flex items-center justify-center w-full h-full
-`;
-
-const Cycle = tw.div`
-  h-16 w-16 border-8 border-t-white border-x-white/30 border-b-white/30 rounded-full animate-spin
-`;
+const LoadingContainer = tw.div``;
