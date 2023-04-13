542346cb2f327f56e2347148d3f2975898b56294
origin1508 <origin1508@gmail.com>
2023-04-13 20:00:24 +0900

    fix: google 로그인버튼 추가

--- components/Login/OAuthLogin.tsx
+++ components/Login/OAuthLogin.tsx
@@ -1,30 +1,40 @@
 import Link from "next/link";
 import Image from "next/image";
 import tw from "twin.macro";
-import Button from "../common/Button";
 
 const OAuthLogin = () => {
   const NAVER_CLIENT_ID = process.env.NEXT_PUBLIC_NAVER_CLIENT_ID;
   const NAVER_CLIENT_STATE = process.env.NEXT_PUBLIC_NAVER_CLIENT_STATE;
 
+  const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
+  const GOOGLE_CLIENT_STATE = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_STATE;
+
   return (
     <OAuthContainer>
       <NaverLogin
-        href={`https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=${NAVER_CLIENT_STATE}&redirect_uri=https://talkgg.online/auth/login/naver`}
+        href={`https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=${NAVER_CLIENT_STATE}&redirect_uri=http://localhost:3002/auth/login/naver`}
       >
-        <NaverIcon
-          src={"/image/naver.png"}
+        <LogoIcon
+          src={"/image/logo_naver.png"}
           width={200}
           height={200}
           quality={100}
           alt="naver"
         />
-        <NaverText>Log in with Naver</NaverText>
+        <LoginText>Sign in with Naver</LoginText>
       </NaverLogin>
-
-      <Button bgColor="secondary" width={18}>
-        Google
-      </Button>
+      <GoogleLogin
+        href={`https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/drive.metadata.readonly&access_type=online&response_type=code&state=${GOOGLE_CLIENT_STATE}&redirect_uri=https://talkgg.online/auth/login/google&client_id=${GOOGLE_CLIENT_ID}`}
+      >
+        <LogoIcon
+          src={"/image/logo_google.png"}
+          width={200}
+          height={200}
+          quality={100}
+          alt="naver"
+        />
+        <LoginText>Sign in with Google</LoginText>
+      </GoogleLogin>
     </OAuthContainer>
   );
 };
@@ -32,17 +42,21 @@ const OAuthLogin = () => {
 export default OAuthLogin;
 
 const OAuthContainer = tw.div`
-  flex w-full flex-col items-center m-auto
+  flex flex-col items-center gap-5 w-full m-auto text-[14px]
+`;
+
+const LogoIcon = tw(Image)`
+  shrink-0 w-[18px] h-[18px] mr-[8px]
 `;
 
 const NaverLogin = tw(Link)`
-  flex items-center justify-center w-3/6 h-12 rounded bg-[#03C75A]
+  flex items-center justify-center w-3/6 h-12 rounded bg-[#03c75a]
 `;
 
-const NaverIcon = tw(Image)`
-  shrink-0 w-12 h-12
+const GoogleLogin = tw(Link)`
+  flex items-center justify-center w-3/6 h-12 rounded bg-white text-black
 `;
 
-const NaverText = tw.div`
+const LoginText = tw.span`
   flex items-center justify-center w-32 text-center
 `;
