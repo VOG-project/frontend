580c1946ae8fa0b2d4ddd6d6fc1c02bf2fe93d65
origin1508 <origin1508@gmail.com>
2023-04-13 20:05:22 +0900

    fix: Loading import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import ErrorBoundary from "@/components/ErrorBoundary";
 import GlobalStyle from "@/styles/GlobalStyle";
 import Toast from "@/components/Toast";
 import Socket from "@/components/Socket/Socket";
+import Loading from "@/components/common/Loading";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -14,6 +15,7 @@ export default function App({ Component, pageProps }: AppProps) {
           <Toast />
           <Component {...pageProps} />
           <Socket />
+          <Loading />
         </RecoilRoot>
       </ErrorBoundary>
     </>
