b1f85c01ea1b7ded02c56a70ff86538d04b0f8f8
origin1508 <origin1508@gmail.com>
2023-04-14 20:08:43 +0900

    fix: 만료일 제거하고 httpOnly 옵션을 true 설정

--- utils/tokenManager.ts
+++ utils/tokenManager.ts
@@ -4,7 +4,7 @@ import { IncomingMessage } from "http";
 const ACCESS_TOKEN = "ACCESS_TOKEN";
 
 const setAccessToken = (accessToken: string) => {
-  setCookie(ACCESS_TOKEN, accessToken, { maxAge: 60 * 60 * 24 * 1000 });
+  setCookie(ACCESS_TOKEN, accessToken, { httpOnly: true });
 };
 
 const getAccessToken = (
