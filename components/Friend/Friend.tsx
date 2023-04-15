739d7021b5f8d0a4ceefca8128debd7dcd61f1ae
origin1508 <origin1508@gmail.com>
2023-04-15 20:50:27 +0900

    fix: Context 컴포넌트 분리

--- components/Friend/Friend.tsx
+++ components/Friend/Friend.tsx
@@ -1,28 +1,20 @@
-import { MouseEvent, FormEvent, useState, useEffect } from "react";
-import tw, { styled, css } from "twin.macro";
+import { FormEvent, useEffect } from "react";
+import tw, { styled } from "twin.macro";
 import useToast from "@/hooks/useToast";
 import useFriendState from "@/hooks/useFriendState";
 import useUserProfileState from "@/hooks/useUserProfileState";
 import useUserState from "@/hooks/useUserState";
 import Header from "../common/Header";
-import UserCard from "../common/UserCard";
+import FriendContext from "./FriendContext";
 import { getIcons } from "../icons";
 import { searchFriendRequest } from "@/apis/friend";
-import { CONTEXT_MENU_WIDTH } from "@/constants/friend";
-import { INITIAL_STATE } from "@/types/friend";
 
 const Friend = () => {
   const { toast } = useToast();
-  const {
-    friends,
-    isShow,
-    handleFriendToggle,
-    handleRemoveFriendClick,
-    updateFriendList,
-  } = useFriendState();
+  const { friends, isShow, handleFriendToggle, updateFriendList } =
+    useFriendState();
   const { handleUserProfileOpen } = useUserProfileState();
   const { userId } = useUserState();
-  const [contextMenuState, setContextMenuState] = useState(INITIAL_STATE);
 
   useEffect(() => {
     if (userId) updateFriendList(userId);
@@ -47,34 +39,6 @@ const Friend = () => {
     target.nickname.value = "";
   };
 
-  const handleRightClick = (e: MouseEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    const maxWidth = window.innerWidth;
-    if (e.pageX + CONTEXT_MENU_WIDTH > maxWidth) {
-      setContextMenuState((prev) => {
-        return {
-          isShow: !prev.isShow,
-          x: maxWidth - e.pageX,
-          y: e.pageY,
-          reverse: true,
-        };
-      });
-    } else {
-      setContextMenuState((prev) => {
-        return {
-          isShow: !prev.isShow,
-          x: e.pageX,
-          y: e.pageY,
-          reverse: false,
-        };
-      });
-    }
-  };
-
-  const resetContextMenuState = () => {
-    setContextMenuState(INITIAL_STATE);
-  };
-
   return (
     <FriendContainer isShow={isShow}>
       <Header title="친구목록" />
@@ -82,40 +46,7 @@ const Friend = () => {
         {friends.map((friend) => {
           return (
             <FriendProfile key={friend.following.id}>
-              <ContextWrapper onContextMenu={(e) => handleRightClick(e)}>
-                {contextMenuState.isShow && (
-                  <ContextMenus
-                    x={contextMenuState.x}
-                    y={contextMenuState.y}
-                    reverse={contextMenuState.reverse}
-                  >
-                    <ContextMenu
-                      onClick={async () => {
-                        await handleUserProfileOpen(friend.userId);
-                        resetContextMenuState();
-                      }}
-                    >
-                      프로필
-                    </ContextMenu>
-                    <ContextMenu>채팅방 초대</ContextMenu>
-                    <ContextMenu
-                      onClick={async () => {
-                        await handleRemoveFriendClick(
-                          friend.userId,
-                          friend.following.id
-                        );
-                        resetContextMenuState();
-                      }}
-                    >
-                      친구 삭제
-                    </ContextMenu>
-                  </ContextMenus>
-                )}
-                <UserCard
-                  nickname={friend.following.nickname}
-                  profilePic={friend.following.profileUrl}
-                />
-              </ContextWrapper>
+              <FriendContext friend={friend} />
             </FriendProfile>
           );
         })}
@@ -153,28 +84,6 @@ const FriendAddBtn = tw.button`
 `;
 const FriendToggle = tw.button`
   absolute p-2 top-1/2 -left-8
-`;
-
-const ContextWrapper = tw.div``;
-
-const ContextMenus = styled.ul<{ x: number; y: number; reverse: boolean }>(
-  ({ x, y, reverse }) => [
-    tw`fixed w-24 divide-y bg-white text-black `,
-    reverse
-      ? css`
-          top: ${y}px;
-          right: ${x}px;
-        `
-      : css`
-          top: ${y}px;
-          left: ${x}px;
-        `,
-  ]
-);
-
-const ContextMenu = tw.li`
-  p-2 cursor-pointer
-  hover:(bg-primary text-white)
 `;
 
 const FriendSearch = tw.form`
