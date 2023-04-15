c17effb6590f110ef1d5f16c3585c134034e6ce2
origin1508 <origin1508@gmail.com>
2023-04-15 20:49:27 +0900

    fix Friend, FriendState, FriendContextProps 타입 추가

--- types/friend.ts
+++ types/friend.ts
@@ -4,3 +4,25 @@ export const INITIAL_STATE: {
   y: number;
   reverse: boolean;
 } = { isShow: false, x: 0, y: 0, reverse: false };
+
+export interface Friend {
+  userId: number;
+  following: {
+    createdAt: string;
+    email: string;
+    id: number;
+    nickname: string;
+    profileUrl: string;
+    sex: "남" | "여";
+    updatedAt: string;
+  };
+}
+
+export interface FriendState {
+  isShow: boolean;
+  friends: Friend[];
+}
+
+export interface FriendContextProps {
+  friend: Friend;
+}
