14a3938935746e74fac4b1c6702606e721767bf3
origin1508 <origin1508@gmail.com>
2023-04-15 20:48:31 +0900

    fix: FriendState 타입 따로 관리

--- recoil/atoms/friendState.ts
+++ recoil/atoms/friendState.ts
@@ -1,27 +1,12 @@
 import { atom } from "recoil";
 import { recoilPersist } from "recoil-persist";
 import { sessionStorage } from "@/utils/sessionStorage";
+import { FriendState } from "@/types/friend";
 
 const { persistAtom } = recoilPersist({
   key: "FRIENDS",
   storage: sessionStorage,
 });
-
-interface FriendState {
-  isShow: boolean;
-  friends: {
-    userId: number;
-    following: {
-      createdAt: string;
-      email: string;
-      id: number;
-      nickname: string;
-      profileUrl: string;
-      sex: "남" | "여";
-      updatedAt: string;
-    };
-  }[];
-}
 
 export const friendState = atom<FriendState>({
   key: "Friend",
