603474c5c343d0f634a57e18012ca781c77b50d5
origin1508 <origin1508@gmail.com>
2023-04-17 13:44:21 +0900

    fix: 중복 로그인시 토큰 삭제

--- components/Login/Login.tsx
+++ components/Login/Login.tsx
@@ -1,7 +1,26 @@
+import { useEffect } from "react";
 import tw from "twin.macro";
+import { useRouter } from "next/router";
+import useUserState from "@/hooks/useUserState";
+import useFriendState from "@/hooks/useFriendState";
+import useToast from "@/hooks/useToast";
 import OAuthLogin from "./OAuthLogin";
+import { deleteAccessToken } from "@/utils/tokenManager";
 
 const Login = () => {
+  const router = useRouter();
+  const { resetUser } = useUserState();
+  const { resetFriend } = useFriendState();
+  const { toast } = useToast();
+  useEffect(() => {
+    if (router.query.authorized) {
+      resetUser();
+      resetFriend();
+      deleteAccessToken();
+      toast.alert("다른 컴퓨터에서 로그인 되었습니다.");
+    }
+  }, []);
+
   return (
     <LoginWrapper>
       <LoginContainer>
@@ -19,7 +38,9 @@ relative flex items-center justify-center h-full bg-[url("./image/valorant.jpg")
 after:(absolute inset-0 bg-black/50)
 `;
 
-const LoginContainer = tw.div`py-10 w-[28rem] rounded drop-shadow bg-black/80 z-10`;
+const LoginContainer = tw.div`
+  py-10 px-20 w-[28rem] rounded drop-shadow bg-black/80 z-10
+`;
 
 const LoginTitle = tw.h2`
   w-full mb-4 text-3xl font-bold text-center
